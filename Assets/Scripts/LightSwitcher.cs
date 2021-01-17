b17087f223629fd9e03504487d516df2d30a2801
AlenaYatkina <yatkinalyona@gmail.com>
2021-01-17 21:43:27 +0300

    Traffic Light Controller
    - adds traffic controller;
    - some visual changes made;

---
+++ Assets/Scripts/LightSwitcher.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitcher : MonoBehaviour
+{
+    //поля для цветов светофора
+    [SerializeField] private TrafficLightsController redLight;
+    [SerializeField] private TrafficLightsController yellowLight;
+    [SerializeField] private TrafficLightsController greenLight;
+
+    //поля для контроля скорости переключения цветов
+    [SerializeField] float RedLightTimer;
+    [SerializeField] float YellowLightTimer;
+    [SerializeField] float GreenLightTimer;
+
+    //Переключение лампочек по таймерам
+    public void Start()
+    {
+        StartCoroutine(switcher());
+    }
+
+    IEnumerator switcher ()
+    {
+        while (true)
+        {
+            GetRedOn();
+            yield return new WaitForSeconds(RedLightTimer);
+
+            GetYellowOn();
+            yield return new WaitForSeconds(YellowLightTimer);
+
+            GetGreenOn();
+            yield return new WaitForSeconds(GreenLightTimer);
+
+            GetYellowOn();
+            yield return new WaitForSeconds(YellowLightTimer);
+        }
+        
+    }
+    //Структура переключения лампочек светофора
+    public void GetRedOn()
+    {
+        redLight. TurnOn();
+
+        yellowLight.TurnOff();
+        greenLight.TurnOff();
+        Debug.Log("Горит красный свет");
+    }
+
+    public void GetYellowOn()
+    {
+        yellowLight.TurnOn();
+
+        redLight.TurnOff();
+        greenLight.TurnOff();
+        Debug.Log("Горит желтый свет");
+    }
+
+    public void GetGreenOn()
+    {
+        greenLight.TurnOn();
+
+        redLight.TurnOff();
+        yellowLight.TurnOff();
+        Debug.Log("Горит зеленый свет");
+    }
+}
---
+++ Assets/Scripts/TrafficLightsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Переключает цвета светофора
+
+public class TrafficLightsController : MonoBehaviour
+{
+    
+    //Поля, которые позволяют присвоить цвет лампочкам светофора
+    [SerializeField] Material onLight;
+    [SerializeField] Material offLight;
+
+
+    private Renderer lightsRenderer;
+
+    private void Awake()
+    {
+        lightsRenderer = GetComponent<Renderer>();
+
+        TurnOff();
+    }
+
+    public void TurnOn()
+    {
+        lightsRenderer.material = onLight;
+    }
+    public void TurnOff()
+    {
+        lightsRenderer.material = offLight ;
+
+    }
+   
+
+}
